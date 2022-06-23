6a7c0edf99458d989f06b2122280b1e69b74422c
aaron saunders <aaron@clearlyinnovative.com>
2022-06-23 16:35:27 -0400

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  modules: ['nuxt-ionic'],
+  ionic: {
+    // integrations: {
+    //   meta: true,
+    //   pwa: true,
+    //   router: true,
+    // },
+    // css: {
+    //   core: true,
+    //   basic: true,
+    //   utilities: false,
+    // },
+  },
+})
